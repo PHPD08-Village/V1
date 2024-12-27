919d4d22fdeb486b68fa13af9c025a46da708434
PHPD08-Village <fakesugarvillage@gmail.com>
2024-12-27 11:02:05 +0800

    新增LINE驅動註解

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        // 監聽 SocialiteWasCalled 事件，來註冊 Line 提供者
+        // 監聽 SocialiteWasCalled 事件，來註冊 Line 提供者 LINE驅動的部分
         $this->app->make('events')->listen(
             SocialiteWasCalled::class,
             \SocialiteProviders\Line\LineExtendSocialite::class
